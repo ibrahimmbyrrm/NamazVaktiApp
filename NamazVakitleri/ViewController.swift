da250da038e3ec2700adf58a1210e798fc177af4
ibrahimmbyrrm <ibrahim_bayram22@yahoo.com>
2023-12-16 18:24:30 +0300

    Initial Commit

---
+++ NamazVakitleri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NamazVakitleri
+//
+//  Created by İbrahim Bayram on 16.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
